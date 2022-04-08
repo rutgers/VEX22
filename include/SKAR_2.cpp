e7f76e47a94ec0830d71428f9c5dd5a0ed007546
Jack Lowry <jacklowry712@gmail.com>
2022-04-07 22:41:54 -0400

    tilter oops

--- include/SKAR_2.cpp
+++ include/SKAR_2.cpp
@@ -132,17 +132,11 @@ void autonomous()
 	lift_front_control->tarePosition();
 	chassis->stop();
 	chassis->setMaxVelocity(200);
-<<<<<<< HEAD
-	if(selector::auton == 0) {
-		// int move_vel = 150;	
-		// chassis->setMaxVelocity(move_vel);
-=======
 	if (selector::auton == 0)
 	{
 		/* int move_vel = 60;
 =======
 		int move_vel = 60;
->>>>>>> a1b3b8bf10bb7b7f6343c73bdd30afc5f83c2ea4
 		chassis->setMaxVelocity(move_vel);
 
 		back_claw_control->setTarget(BACK_CLAW_DOWN);
@@ -154,7 +148,6 @@ void autonomous()
 		// pros::delay(250);
 		// back_claw_control->setTarget(0);
 
-<<<<<<< HEAD
 		// // Grab Our Side
 		// lift_front_control->setTarget(FRONT_LIFT_DOWN);
 		// chassis->moveDistance(1.75_ft);
@@ -176,53 +169,6 @@ void autonomous()
 		// pros::delay(1000);
 		// chassis->setMaxVelocity(50);
 		// chassis->moveDistanceAsync(-2_ft);
-=======
-		// Grab Opposing side on Balance
-		chassis->turnAngle(-40_deg);
-		chassis->moveDistance(1.5_ft);
-		lift_front_control->setTarget(FRONT_LIFT_PLAT);
-		pros::delay(1000);
-		chassis->turnAngle(-200_deg);
-		turn_to_goal(camera, drive_lft, drive_rt, BLUE);
-		chassis->moveDistance(0.75_ft);
-		lift_back_control->setTarget(BACK_LIFT_DOWN);
-		pros::delay(1000);
-		chassis->setMaxVelocity(50);
-		chassis->moveDistanceAsync(-2_ft);
-		pros::delay(3000);
-		chassis->stop();
-		chassis->setMaxVelocity(move_vel);
-		lift_back_control->setTarget(BACK_LIFT_UP);
-		back_claw_control->setTarget(BACK_CLAW_DOWN);
-		chassis->moveDistance(1.2_ft);
-
-		// Place Our Side
-		chassis->turnAngle(135_deg);
-		intake->moveVoltage(INTAKE_IN);
-		chassis->moveDistance(2.75_ft);
-		chassis->turnAngle(90_deg);
-		chassis->moveDistanceAsync(1.25_ft);
-		pros::delay(2000);
-		open_claw(front_claw_piston, front_claw_control);
-		pros::delay(1000);
-		intake->moveVoltage(0);
-
-		// Grab Yellow
-		chassis->moveDistance(-0.5_ft);
-		chassis->turnAngle(-100_deg);
-		chassis->moveDistance(-1.75_ft);
-		chassis->turnAngle(-80_deg);
-		lift_front_control->setTarget(FRONT_LIFT_DOWN);
-		pros::delay(1000);
-		chassis->moveDistance(3.5_ft);
-		close_claw(front_claw_piston, front_claw_control);
-		lift_front_control->setTarget(FRONT_LIFT_PLAT);
-
-		// Move to balance
-		chassis->moveDistance(4_ft);
-		chassis->turnAngle(-60_deg);
-		chassis->moveDistanceAsync(4_ft); */
->>>>>>> 64ad5341b52c3f4d5b257be47caeae29e195162e
 		// pros::delay(3000);
 		// chassis->stop();
 		// chassis->setMaxVelocity(move_vel);
@@ -277,12 +223,6 @@ void autonomous()
 
 		//Grab Yellow
 		int DIST = 35;
-<<<<<<< HEAD
-=======
-		// Grab Yellow
-		/* int DIST = 28;
-		int DIST = 28;
->>>>>>> eb98309ef59019eb3bcb62c45c5826fe84bb1bce
 >>>>>>> a1b3b8bf10bb7b7f6343c73bdd30afc5f83c2ea4
 >>>>>>> 64ad5341b52c3f4d5b257be47caeae29e195162e
 		drive_rt->moveVoltage(12000);
@@ -373,19 +313,11 @@ void autonomous()
 	}
 }
 
-<<<<<<< HEAD
-// void autonomous()
-// {
-// 	//balance(chassis, imu);
-// 	imu_turning(361, drive_rt, drive_lft, imu);
-// }
-=======
 /* void autonomous1()
 {
 	balance(drive_rt, drive_lft, imu);
 	// imu_turning(361, drive_rt, drive_lft, imu);
 } */
->>>>>>> 64ad5341b52c3f4d5b257be47caeae29e195162e
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -512,6 +444,7 @@ void opcontrol()
 
 		front_claw_piston->set_value(front_flag);
 		back_claw_piston->set_value(back_flag);
+		back_tilter->set_value(back_flag);
 
 		// if(master->get_digital(DIGITAL_Y)) {
 		// 	turn_to_goal(camera, drive_lft, drive_rt, AUTON_COLOR);
